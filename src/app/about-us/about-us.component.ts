ee0de50b471d1a8e22867a6180d0e87c6e3acf76
Francisc <szantofrancisc08@gmail.com>
2024-11-27 15:26:38 +0200

    about-us component done

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from '../home-page/home-page.component';
+import { ContactComponent } from '../contact/contact.component';
+import { ServicesComponent } from '../services/services.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
+  sidemenuStyle: any;
 
+  constructor() { }
+
+  openMenu() {
+    // Logic for opening the menu
+    console.log("Opening menu");
+    // Example: Set a style to show the menu
+    this.sidemenuStyle = { 'display': 'block' };
+  }
+
+  closeMenu() {
+    // Logic for closing the menu
+    console.log("Closing menu");
+    // Example: Set a style to hide the menu
+    this.sidemenuStyle = { 'display': 'none' };
+  }
 }
+
+export const routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+];
